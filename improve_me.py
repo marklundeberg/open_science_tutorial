352b380bb955b52d7c9f1a59fe23728b3a2bcbf4
Mark <nanite@gmail.com>
2015-06-04 15:43:14 +0200

    improve_me.py Much better now.

--- improve_me.py
+++ improve_me.py
@@ -1,23 +1,2 @@
-# The header is missing and there are unnecessary imports
-import random, getopt, sys, os
+print("This script is perfect")
 
-# This line will only work in Python 2
-print "This script is not perfect"
-
-# The following loop could be written in a single line with list comprehension
-random_numbers = []
-for i in range(10):
-    random_numbers.append(random.random())
-
-# Don't do this. Just don't.
-for i in range(len(random_numbers)):
-    print random_numbers[i]
-
-# This loop could be rewritten with enumerate
-i = 0
-for r in random_numbers:
-    print(i, r)
-    i += 1
-
-# This is not C
-if(len(random_numbers)>5): print("Lots of random numbers here")
